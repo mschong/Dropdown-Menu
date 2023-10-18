a6e3a2804c66362cc6b35540bb0ed6965612ec7a
Marina Chong <marinachong96@gmail.com>
2023-10-17 19:46:16 -0700

    Added Select All button in multiselect option

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ const options = [
   {label: "Three", value: "3"},
   {label: "Four", value: "4"},
   {label: "Five", value: "5"},
-  {label: "Six", value: "6"},
+  {label: "Six", value: "6"}
 ]
 function App() {
 
   return (
-    <DropdownMenu options={options} isSingleSelect={false} defaultValue={"Hello"}>
-    </DropdownMenu>
+    <DropdownMenu options={options} isSingleSelect={false} defaultValue={"Hello"} />
   )
 }
 
--- src/Dropdown.js
+++ src/Dropdown.js
@@ -5,13 +5,15 @@ export function DropdownMenu(props) {
     const [displayedValues, setDisplayedValues] = useState(defaultValue);
     const [values, setValues] = useState([]);
     const [isOpen, setIsOpen] = useState(false);
+    const [checkBox, setCheckBox] = useState([]);
+    const [isCheckAll, setIsCheckAll] = useState(false);
 
     function setValue(value) {
         if (isSingleSelect) {
             setDisplayedValues(value.label);
         } else {
             const newValues = values.slice();
-            if(newValues.includes(value)){
+            if (newValues.includes(value)) {
                 newValues.splice(newValues.indexOf(value), 1);
             } else {
                 newValues.push(value);
@@ -22,36 +24,81 @@ export function DropdownMenu(props) {
         }
     }
 
+    function handleSelectAll() {
+        setIsCheckAll(!isCheckAll);
+        if(isCheckAll) {
+            setCheckBox([])
+            setDisplayedValues("");
+        } else {
+            setCheckBox(options.map(option => option.value));
+            const allOptions = options.map(option => option.label);
+            setValues(options);
+            setDisplayedValues(allOptions.join(", "));
+        }
+    }
+
+   function handleCheck(e){
+        const { id, checked } = e.target;
+        if(checked){
+            setCheckBox([...checkBox, id]);
+        } else {
+            setCheckBox(checkBox.filter(item => item !== id))
+        }
+    }
+
     return (
-        <div tabIndex={0} onClick={() => setIsOpen(!isOpen)} className="container">
-            <span className="value">{displayedValues}</span>
-            {isOpen ? (
-                <div className="arrow-container"><i className="arrow up"></i></div>
-            ) : (
-                <div className="arrow-container"><i className="arrow down"></i></div>
-            )}
+        <div tabIndex={0} className="container">
+            <span className="value" onClick={() => setIsOpen(!isOpen)}>{displayedValues}</span>
+            <div className="arrow-container" onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? (
+                    <i className="arrow up"></i>
+                ) : (
+                    <i className="arrow down"></i>
+                )}
+            </div>
             <ul className={`${"options"} ${isOpen ? "open" : ""}`}>
-                    {options.map(option => (
-                        isSingleSelect ? (
+                {isSingleSelect ? (
+                    <></>
+                ) : (
+                    <li key="selectAll" className="options-item">
+                        <Checkbox id="selectAll" name="selectAll" isChecked={isCheckAll} handleClick={handleSelectAll} />
+                        <label>Select All</label>
+                    </li>
+                )}
+                {options.map(option => (
+                    isSingleSelect ? (
                         <li key={option.value} className="options-item"
                             onClick={e => {
                                 setValue(option)
+                                setIsOpen(!isOpen)
                             }}
                         >
                             {option.label}
                         </li>
-                        ) : (
-                            <li key={option.value} className="options-item">
-                            <input
-                                type="checkbox"
+                    ) : (
+                        <li key={option.value} className="options-item">
+                            <Checkbox
+                                id={option.value}
+                                name={option.label}
                                 value={option.value}
-                                onChange={e => { setValue(option) }}
+                                isChecked={checkBox.includes(option.value)}
+                                handleClick={e => {
+                                    setValue(option)
+                                    handleCheck(e)
+                                }}
                             />
                             {option.label}
                         </li>
-                        )
-                    ))}
-                </ul>
+                    )
+                ))}
+            </ul>
         </div>
+    )
+}
+
+function Checkbox(props) {
+    const { id, name, isChecked, handleClick } = props;
+    return (
+        <input id={id} type="checkbox" name={name} checked={isChecked} onChange={handleClick} />
     )
 }
